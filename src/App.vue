6f1a3b2fd415c473b311937d0d6f86ba54976b71
122787987@qq.com <122787987@qq.com>
2023-04-09 15:50:30 +0800

    feat: addH1

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
-  <div></div>
+  <div>
+    <h1>我新增一个标题</h1>
+  </div>
 </template>
 
 <script setup lang="ts">
-for (let i = 0; i < 100; i++) {
-  console.log(i)
-}
 
-let str = '我是祖国的老花骨朵'
 </script>
 
-<style scoped></style>
+<style scoped>
+
+</style>
