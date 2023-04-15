4e2cbc0104c9240eda941c0f5fd1a3cecd68f810
122787987@qq.com <122787987@qq.com>
2023-04-15 14:13:18 +0800

    fix: add login userinfo logout

--- src/layout/tabbar/setting/index.vue
+++ src/layout/tabbar/setting/index.vue
@@ -49,11 +49,11 @@ const fullScreen = () => {
     }
 }
 //退出登录点击回调
-const logout = () => {
+const logout = async () => {
     //第一件事情:需要向服务器发请求[退出登录接口]******
     //第二件事情:仓库当中关于用于相关的数据清空[token|username|avatar]
     //第三件事情:跳转到登录页面
-     userStore.userLogout();
+    await userStore.userLogout();
     //跳转到登录页面
     $router.push({ path: '/login', query: { redirect: $route.path } });
 
